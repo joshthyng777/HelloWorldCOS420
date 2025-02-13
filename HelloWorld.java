15fff746cc7ab588c6d0ee67d609dd7ce43ba153
Josh T <joshthyng187@gmail.com>
2025-02-12 21:10:57 -0500

    added free memory output which closes issue 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,8 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Current date: " + LocalDate.now());
+        System.out.println("Free memory: " + Runtime.getRuntime().freeMemory() + " bytes");
     }
 }
+
 
